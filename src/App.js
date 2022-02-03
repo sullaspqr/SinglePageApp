c2b387a68baba0f38e5092547077348acdd08193
sullaspqr <sulla.spqr78@gmail.com>
2022-02-03 13:20:42 +0100

    RRD 6.2.1

--- src/App.js
+++ src/App.js
@@ -1,38 +1,34 @@
+import { BrowserRouter, NavLink, Routes, Route } from "react-router-dom";
+import  { InstrumentListPage } from "./InstrumentListPage";
+import { InstrumentSinglePage } from "./InstrumentSinglePage";
 import './App.css';
-import { BrowserRouter, NavLink, Redirect, Route, Switch } from "react-router-dom";
-import {InstrumentListPage} from "./InstrumentListPage";
+
 function App() {
   return (
     <BrowserRouter>
-    <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <NavLink to={'/'} activeClassName="active" exact>
-              <span className="nav-link">Hangszerek</span>
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink to={'/uj-hangszer'} activeClassName="active">
-              <span className="nav-link">Új hangszer</span>
-            </NavLink>
-          </li>
-        </ul>
-      </div>
-    </nav>
-    <Switch>
-      <Route path="/" exact component={InstrumentListPage} />
-
-      <Route path="/hangszer/:hangszerId">
-      Hangszer aloldal
-      </Route>
-      <Route path="/uj-hangszer">
-      Új hangszer létrehozó oldal
-      </Route>
-      <Route>
-        <Redirect to={"/"} />
-      </Route>
-    </Switch>
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink to={'/'} className={({isActive}) => "nav-link" + (isActive ? "active" : "")} end>
+                <span className="nav-link">Hangszerek</span>
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to={'/uj-hangszer'} className={({isActive}) => "nav-link" + (isActive ? "active" : "")}>
+                <span className="nav-link">Új hangszer</span>
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <Routes>
+        <Route path="/" exact element={<InstrumentListPage />} />
+        <Route path="/hangszer/:hangszerId" element={<InstrumentSinglePage />} />
+        <Route path="/uj-hangszer">
+          Új hangszer létrehozó oldal
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
