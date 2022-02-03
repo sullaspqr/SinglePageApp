3e13576ff4f098961c37591d6fc6ed7ba461d7fd
sullaspqr <sulla.spqr78@gmail.com>
2022-02-03 10:57:08 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter, NavLink, Redirect, Route, Switch } from "react-router-dom";
+import {InstrumentListPage} from "./InstrumentListPage";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <NavLink to={'/'} activeClassName="active" exact>
+              <span className="nav-link">Hangszerek</span>
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to={'/uj-hangszer'} activeClassName="active">
+              <span className="nav-link">Új hangszer</span>
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <Switch>
+      <Route path="/" exact component={InstrumentListPage} />
+
+      <Route path="/hangszer/:hangszerId">
+      Hangszer aloldal
+      </Route>
+      <Route path="/uj-hangszer">
+      Új hangszer létrehozó oldal
+      </Route>
+      <Route>
+        <Redirect to={"/"} />
+      </Route>
+    </Switch>
+    </BrowserRouter>
   );
 }
 
